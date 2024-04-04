539c02e09406756c346b4b798536d0a5d72a32ee
Arto Bendiken <arto@bendiken.net>
2024-04-04 11:42:35 +0400

    Add some missing sending methods.

--- lib/dart_port/src/port.rs
+++ lib/dart_port/src/port.rs
@@ -5,7 +5,7 @@ use dart_sys::{
     Dart_CObject, Dart_CloseNativePort_DL, Dart_Port_DL, Dart_PostCObject_DL, Dart_PostInteger_DL,
 };
 use std::{
-    ffi::CString,
+    ffi::{CStr, CString},
     mem::size_of,
     sync::atomic::{AtomicI64, Ordering},
 };
@@ -81,7 +81,7 @@ impl DartPort {
 impl SendPort for DartPort {
     #[track_caller]
     fn post_null(&self) -> Result<()> {
-        self.post_cobject(&mut Dart_CObject {
+        self.post_cobject(Dart_CObject {
             type_: dart_sys::Dart_CObject_Type_Dart_CObject_kNull,
             value: dart_sys::_Dart_CObject__bindgen_ty_1 { as_bool: false },
         })
@@ -89,9 +89,17 @@ impl SendPort for DartPort {
 
     #[track_caller]
     fn post_bool(&self, value: bool) -> Result<()> {
-        self.post_cobject(&mut Dart_CObject {
+        self.post_cobject(Dart_CObject {
             type_: dart_sys::Dart_CObject_Type_Dart_CObject_kBool,
             value: dart_sys::_Dart_CObject__bindgen_ty_1 { as_bool: value },
+        })
+    }
+
+    #[track_caller]
+    fn post_double(&self, value: f64) -> Result<()> {
+        self.post_cobject(Dart_CObject {
+            type_: dart_sys::Dart_CObject_Type_Dart_CObject_kDouble,
+            value: dart_sys::_Dart_CObject__bindgen_ty_1 { as_double: value },
         })
     }
 
@@ -115,7 +123,7 @@ impl SendPort for DartPort {
 
     #[track_caller]
     fn post_integers(&self, values: &[i64]) -> Result<()> {
-        self.post_cobject(&mut Dart_CObject {
+        self.post_cobject(Dart_CObject {
             type_: dart_sys::Dart_CObject_Type_Dart_CObject_kTypedData,
             value: dart_sys::_Dart_CObject__bindgen_ty_1 {
                 as_typed_data: dart_sys::_Dart_CObject__bindgen_ty_1__bindgen_ty_4 {
@@ -135,8 +143,8 @@ impl SendPort for DartPort {
     }
 
     #[track_caller]
-    fn post_cstring(&self, value: &CString) -> Result<()> {
-        self.post_cobject(&mut Dart_CObject {
+    fn post_cstr(&self, value: &CStr) -> Result<()> {
+        self.post_cobject(Dart_CObject {
             type_: dart_sys::Dart_CObject_Type_Dart_CObject_kString,
             value: dart_sys::_Dart_CObject__bindgen_ty_1 {
                 as_string: value.as_ptr() as _,
@@ -145,14 +153,23 @@ impl SendPort for DartPort {
     }
 
     #[track_caller]
-    fn post_cobject(&self, value: &mut Dart_CObject) -> Result<()> {
+    fn post_cstring(&self, value: &CString) -> Result<()> {
+        self.post_cobject(Dart_CObject {
+            type_: dart_sys::Dart_CObject_Type_Dart_CObject_kString,
+            value: dart_sys::_Dart_CObject__bindgen_ty_1 {
+                as_string: value.as_ptr() as _,
+            },
+        })
+    }
+
+    fn post_cobject(&self, mut value: Dart_CObject) -> Result<()> {
         let port_id = self.id.load(ATOMIC_ORDERING);
         if port_id == ILLEGAL_PORT_ID {
             return Err(Error::IllegalPort);
         }
         unsafe {
             let post = Dart_PostCObject_DL.expect("Dart API initialized");
-            if !post(port_id, value) {
+            if !post(port_id, &mut value) {
                 // See: PortMap::PostMessage in sdk/runtime/vm/port.cc
                 // See: https://github.com/dart-lang/sdk/blob/3.3.3/runtime/vm/port.cc#L152
                 // See: https://github.com/dart-lang/sdk/blob/3.3.3/runtime/vm/native_api_impl.cc#L59
--- lib/dart_port/src/send_port.rs
+++ lib/dart_port/src/send_port.rs
@@ -2,15 +2,17 @@
 
 use super::prelude::Result;
 use dart_sys::Dart_CObject;
-use std::ffi::CString;
+use std::ffi::{CStr, CString};
 
 /// See: https://api.dart.dev/stable/3.3.3/dart-isolate/SendPort-class.html
 pub trait SendPort {
     fn post_null(&self) -> Result<()>;
     fn post_bool(&self, value: bool) -> Result<()>;
+    fn post_double(&self, value: f64) -> Result<()>;
     fn post_integer(&self, value: i64) -> Result<()>;
     fn post_integers(&self, values: &[i64]) -> Result<()>;
     fn post_string(&self, value: impl AsRef<str>) -> Result<()>;
+    fn post_cstr(&self, value: &CStr) -> Result<()>;
     fn post_cstring(&self, value: &CString) -> Result<()>;
-    fn post_cobject(&self, value: &mut Dart_CObject) -> Result<()>;
+    fn post_cobject(&self, value: Dart_CObject) -> Result<()>;
 }
